fdf73910a27e499f11d822702d476101326fe0e9
niccolocorraro <nic.corraro@stud.uniroma3.it>
2023-10-22 12:11:18 +0200

    fix vite

--- Assets/Player/Health.cs
+++ Assets/Player/Health.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     private Animator anim;
     private Rigidbody2D rb;
     [SerializeField] private float startingHealth;
     public float currentHealth{ get; private set; }
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Start()
     {
@@ -24,8 +30,8 @@ public class Health : MonoBehaviour
             currentHealth = Mathf.Clamp(currentHealth - 1, 0, startingHealth);
 
             if(currentHealth > 0){
-                currentHealth = currentHealth-1;
                 anim.SetTrigger("hurtTrigger");
+                 StartCoroutine(Invunerability());
             }
             else{
                 Die();
@@ -46,5 +52,17 @@ public class Health : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+
+       private IEnumerator Invunerability(){
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
   
 }
