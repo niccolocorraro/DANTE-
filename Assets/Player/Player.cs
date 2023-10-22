163f9d9c4f8aca58f2f62ba3cb1cb5ea67047a0d
niccolocorraro <nic.corraro@stud.uniroma3.it>
2023-10-22 16:28:58 +0200

    fix rotation

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -42,7 +42,7 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
         InputProc();
 
     }
