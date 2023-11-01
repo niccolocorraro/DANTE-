789b82619cb4de0def24ac53a2c0695bc83e4a03
ErCrozzUni <leo.crozzoli@stud.uniroma3.it>
2023-11-02 00:27:13 +0100

    avanti con i menu

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+      public void PlayGame()
+    {
+        SceneManager.LoadScene(""); 
+        //se vogliamo passare invece alla prossima scena perché numerate allora scriviamo 
+        //SceneManager.GetActiveScene().buildIndex + 1 
+        //le scene si numerano nel build settings di unity 
+    }
+}
