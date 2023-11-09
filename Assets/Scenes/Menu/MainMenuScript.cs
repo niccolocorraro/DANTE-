febc3fb799afa47db4902632dfdfe40fb6912479
ErCrozzUni <leo.crozzoli@stud.uniroma3.it>
2023-11-09 10:30:48 +0100

    ADESSO FUNZIONA MENU
    e anche i pulsanti ziopera
    manca solo i tasti del volume quando mettiamo audio

--- Assets/Scenes/Menu/MainMenuScript.cs
+++ Assets/Scenes/Menu/MainMenuScript.cs
@@ -7,7 +7,7 @@ public class MainMenuScript : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(""); 
+        SceneManager.LoadScene(1); 
         //se vogliamo passare invece alla prossima scena perché numerate allora scriviamo 
         //SceneManager.GetActiveScene().buildIndex + 1 
         //le scene si numerano nel build settings di unity 
